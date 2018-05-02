c234e536b3308e65f46ea61b23d0998a3efef09d
Tobito1916 <jonas.hoesl@gmx.de>
2018-05-02 17:22:31 +0200

    Bufferedreader 1

--- src/Read.java
+++ src/Read.java
@@ -1,5 +1,13 @@
+import java.io.*;
 public class Read {
     String readin(){
+        try (BufferedReader in = new BufferedReader(new FileReader("chiffrat.txt"))) {
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return "none";
     }
 }
